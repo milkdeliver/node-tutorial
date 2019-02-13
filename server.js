378ddfa132d8445fc019f4ca59ae677dc4245951
Sam <sam912358@gmail.com>
2019-02-13 14:42:28 +0800

    upload text

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log('Request for' + pathname + ' recevied');
-        route(handle, pathname, response);
-    }
+        var postData = '';
+
+        request.setEncoding('utf8');
+        request.addListener('data', function(postDataChunk) {
+            postData += postDataChunk;
+            console.log('Received POST data chunk: ' + postDataChunk );
+        });
+
+        request.addListener('end', function() {
+            route(handle, pathname, response, postData);
+        })
+   }
 
     http.createServer(onRequest).listen(8888);
     console.log('Server has started...');
